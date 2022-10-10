774e90f60ba74b06b2fa0de267c8c2717e86646a
preethigopalakrishnan6 <preethigbbt@gmail.com>
2022-10-10 22:10:13 +0530

    Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+class Calculator
+{
+public static void main(String[]args)
+{
+Calculator calc =new Calculator();
+calc.add();
+calc.subtract();
+calc.multiply();
+calc.divide();
+}
+void divide()
+{
+System.out.println(10/20);
+}
+void multiply()
+{
+System.out.println(10*20);
+}
+void subtract()
+{
+System.out.println(10-20);
+}
+void add()
+{
+System.out.println(10+20);
+}
+}   
+
+
+
