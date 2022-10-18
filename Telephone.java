15c450cadbebb90212fbeb1cc9e37f29a2ee4c72
preethigopalakrishnan6 <preethigbbt@gmail.com>
2022-10-18 22:39:04 +0530

    Telephone.java

---
+++ Telephone.java
@@ -0,0 +1,23 @@
+class Telephone
+{
+  int price = 1500;
+  String name="Phone";
+ public void call()
+{
+   System.out.println("Calling Facility");
+}
+public void received_call()
+{
+  System.out.println("Receiving call facility");
+}
+public static void main(String[] args)
+{
+   Telephone bsnl =new Telephone();
+   bsnl.call();
+   bsnl.received_call();
+   System.out.println(bsnl.name);
+   System.out.println(bsnl.price);
+}
+}
+
+
